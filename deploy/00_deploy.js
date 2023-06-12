31fbb5fbc3f4efdf493655ad191485cbd98991d9
Aayush Gupta <guptaaayush432@gmail.com>
2023-06-12 22:38:07 +0530

    [script] remove functions

--- deploy/00_deploy.js
+++ deploy/00_deploy.js
@@ -11,29 +11,30 @@ module.exports = async ({ deployments }) => {
     const { deploy } = deployments;
     console.log("Wallet Ethereum Address:", wallet.address)
     const chainId = network.config.chainId
-    const tokensToBeMinted = networkConfig[chainId]["tokensToBeMinted"]
+    console.log("chainID: ",chainId)
+    // const tokensToBeMinted = networkConfig[chainId]["tokensToBeMinted"]
 
     
-    //deploy Simplecoin
-    const simpleCoin = await deploy("SimpleCoin", {
-        from: wallet.address,
-        args: [tokensToBeMinted],
-        log: true,
-    });
+    // //deploy Simplecoin
+    // const simpleCoin = await deploy("SimpleCoin", {
+    //     from: wallet.address,
+    //     args: [tokensToBeMinted],
+    //     log: true,
+    // });
 
-    //deploy FilecoinMarketConsumer
-    const filecoinMarketConsumer = await deploy("FilecoinMarketConsumer", {
-        from: wallet.address,
-        args: [],
-        log: true,
-    });
+    // //deploy FilecoinMarketConsumer
+    // const filecoinMarketConsumer = await deploy("FilecoinMarketConsumer", {
+    //     from: wallet.address,
+    //     args: [],
+    //     log: true,
+    // });
 
-    //deploy DealRewarder
-    const dealRewarder = await deploy("DealRewarder", {
-        from: wallet.address,
-        args: [],
-        log: true,
-    });
+    // //deploy DealRewarder
+    // const dealRewarder = await deploy("DealRewarder", {
+    //     from: wallet.address,
+    //     args: [],
+    //     log: true,
+    // });
     
     //deploy DealClient
     const dealClient = await deploy("DealClient", {
