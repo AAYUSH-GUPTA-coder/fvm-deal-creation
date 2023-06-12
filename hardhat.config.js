ac8195291f284c6d71041cbe628eb9f91597d3e2
Aayush Gupta <guptaaayush432@gmail.com>
2023-06-12 22:38:29 +0530

    [hardhat.config] corrected config

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,33 +10,28 @@ module.exports = {
     solidity: {
         version: "0.8.17",
         settings: {
-            optimizer: {
-                enabled: true,
-                runs: 1000,
-                details: { yul: false },
-            },
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+            details: { yul: false },
+          },
         },
-    },
-    defaultNetwork: "calibration",
+      },
+    defaultNetwork: "calibrationnet",
     networks: {
         localnet: {
             chainId: 31415926,
             url: "http://127.0.0.1:1234/rpc/v1",
             accounts: [PRIVATE_KEY],
         },
-        hyperspace: {
-            chainId: 3141,
-            url: "https://api.hyperspace.node.glif.io/rpc/v1",
+        calibrationnet: {
+            chainId: 314159,
+            url: "https://api.calibration.node.glif.io/rpc/v1",
             accounts: [PRIVATE_KEY],
         },
         filecoinmainnet: {
             chainId: 314,
             url: "https://api.node.glif.io",
-            accounts: [PRIVATE_KEY],
-        },
-        calibration: {
-            chainId: 314159,
-            url: "https://api.calibration.node.glif.io/rpc/v1",
             accounts: [PRIVATE_KEY],
         },
     },
